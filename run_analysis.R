57cb6cee7693d6723f4e913dc8b071b6bc7cb85f
carlosgee111 <carlos.gee111@gmail.com>
2016-06-10 02:20:31 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,10 @@
+##The submitted data set is tidy.
+##The Github repo contains the required scripts.
+##GitHub contains a code book that modifies and updates the available codebooks with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
+##The README that explains the analysis files is clear and understandable.
+##The work submitted for this project is the work of the student who submitted it.
+
+
 ##Read train and test data
 train_csv = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
 train_csv[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
